e478105473f0dda4ffae0cd9db6f3c49e8b1290c
Gameryeeter213 <pokemonyveltal50@gmail.com>
2026-02-08 00:25:23 -0500

    Window with changing colors

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-gcc src/main.c glad/src/gl.c -Iglad/include -lglfw -ldl -o Engine
+gcc src/main.c glad/src/gl.c -Iglad/include -lglfw -lGL -lX11 -lpthread -lXrandr -lXi -ldl -o Engine
 ./Engine
--- src/main.c
+++ src/main.c
@@ -1,44 +1,53 @@
 #include <stdlib.h>
 #include <stdio.h>
-
-#define GLAD_GL_IMPLEMENTATION // Necessary for headeronly version.
 #include <glad/gl.h>
-
 #include <GLFW/glfw3.h>
 
-
-const GLuint WIDTH = 800, HEIGHT = 600;
-
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, 1);
 }
 
-int main(void) {
+int main()
+{
+    int height = 800; int width = 600;
+    float r = 0;float g = 1;float b = 0.5; float rc = 0.01;float gc = -0.01;float bc = 0.01;
     glfwInit();
-
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "[glad] GL with GLFW", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "GL_Engine", NULL, NULL);
+
     glfwMakeContextCurrent(window);
+    gladLoaderLoadGL();
+    glViewport(0, 0, 800, 600);
 
-    glfwSetKeyCallback(window, key_callback);
+    while(!glfwWindowShouldClose(window))
+    {
+        if (r >= 1){r=-0.01;}
+        else if(r<=0){rc=0.01;}
+        r=r+rc;
 
-    int version = gladLoadGL(glfwGetProcAddress);
-    printf("GL %d.%d\n", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
+        if (g >= 1){g=-0.01;}
+        else if(g<=0){gc=0.01;}
+        g=g+gc;
 
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+        if (b >= 1){b=-0.01;}
+        else if(b<=0){bc=0.01;}
+        b=b+bc;
 
-        glClearColor(0.7f, 0.9f, 0.1f, 1.0f);
+        processInput(window);
+        glClearColor(r, g, b, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+
+
+        glfwPollEvents();
         glfwSwapBuffers(window);
     }
-
-    glfwTerminate();
 
     return 0;
 }
